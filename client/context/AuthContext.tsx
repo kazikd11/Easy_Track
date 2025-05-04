becd2d801c60c1541257ba2f6e85bd85f9defd9b
Michał Kaźmierczak <kmiki246@gmail.com>
2025-05-04 15:56:03 +0200

    fixed AuthContext.tsx

--- client/context/AuthContext.tsx
+++ client/context/AuthContext.tsx
@@ -1,13 +1,12 @@
 import React, {createContext, useContext, useEffect, useState} from "react";
 import {usePopup} from "@/context/PopupContext";
-import {getJwt, getRefreshToken} from "@/utils/jwt";
+import {deleteJwt, deleteRefreshToken, getJwt, getRefreshToken, saveJwt, saveRefreshToken} from "@/utils/jwt";
 
 interface AuthContextType {
     user: string | null;
-    login: (jwt: string) => Promise<void>;
+    login: (jwt: string, refresh: string) => Promise<void>;
     logout: () => Promise<void>;
-    register: (email: string, password: string) => Promise<void>;
-    refresh: () => Promise<void>;
+    refresh: (jwt: string) => Promise<void>;
 }
 
 
@@ -15,38 +14,43 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({children}: { children: React.ReactNode }) => {
     const [user, setUser] = useState<AuthContextType["user"]>(null);
-    const [refresh, setRefresh] = useState<string | null>(null);
     const {showMessage} = usePopup();
 
     //load user on start
     useEffect(() => {
         (async () => {
-            const [token, refreshToken] = await Promise.all([
-                getJwt(),
-                getRefreshToken()
-            ]);
+            const token = await getJwt()
             if (token) setUser(token);
-            if (refreshToken) setRefresh(refreshToken);
         })();
     }, []);
 
 
-    const login = async (jwt: string) => {
-        await saveJwt(jwt).catch((e) => {
-            console.error(e)
-        })
+    const login = async (jwt: string, refresh: string) => {
+        await Promise.all([
+            saveJwt(jwt),
+            saveRefreshToken(refresh)
+        ])
         setUser(jwt);
+        showMessage({text: "Logged in successfully", type: "info"});
     };
 
     const logout = async () => {
-        await deleteJwt().catch((e) => {
-            console.error(e)
-        })
+        await Promise.all([
+            deleteJwt(),
+            deleteRefreshToken()
+        ])
         setUser(null);
+        showMessage({text: "Logged out successfully", type: "info"});
     };
 
+    const refresh = async (jwt: string) => {
+        await saveJwt(jwt);
+        setUser(jwt);
+        console.log("jwt refreshed");
+    }
+
     return (
-        <AuthContext.Provider value={{user, login, logout}}>
+        <AuthContext.Provider value={{user, login, logout, refresh}}>
             {children}
         </AuthContext.Provider>
     );
